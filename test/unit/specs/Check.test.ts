a5327baa064f9d6b1f35f0eed41fccbf9df42e10
javierbrea <javier.brea@gmail.com>
2025-01-03 12:33:37 +0100

    chore: Add binary for checking scaffold. Rename to opensource-scaffold

---
+++ test/unit/specs/Check.test.ts
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2024 Telefónica Innovación Digital and contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { run } from "@src/Check";
+
+describe("check", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should log", () => {
+    const log = jest.spyOn(console, "log").mockImplementation();
+    run();
+
+    expect(log).toHaveBeenCalledWith(
+      "Hello world! This will check the opensource scaffold",
+    );
+  });
+});
