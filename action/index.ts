787fa18024d9a032071e5cf26d75e91a6590fe48
javierbrea <javier.brea@gmail.com>
2025-01-03 14:09:58 +0100

    chore: Fix paths

--- action/index.ts
+++ action/index.ts
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2024 Telefónica Innovación Digital and contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { runAction } from "./main.js";
-runAction();
+import { run } from "./main.js";
+run();
