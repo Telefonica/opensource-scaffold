59fe8d11761fd689ec15fe5002399a08a7a04499
javierbrea <983381+javierbrea@users.noreply.github.com>
2025-01-07 10:05:13 +0000

    Initialize repository

--- bin/opensource-scaffold.mjs
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-// SPDX-FileCopyrightText: 2024 Telefónica Innovación Digital and contributors
-// SPDX-License-Identifier: Apache-2.0
-
-import { run } from "../dist/Cli.js";
-run();
